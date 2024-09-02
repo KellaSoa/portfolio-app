e5bcf107150340d22a9db88b98792df5a4831787
kella <kella@a-piu.it>
2024-09-02 17:18:07 +0200

    🚧  F04 >Skills > logo

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -4,7 +4,7 @@ import Title from "./Title";
 
 export default function Skills() {
   return (
-    <div id="Skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+    <div id="Skills" className="w-full  bg-[#0a192f] text-gray-300">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <Title />
         <List skills={skillConfig} />
--- src/components/skills/skillConfig.ts
+++ src/components/skills/skillConfig.ts
@@ -1,12 +1,19 @@
-import HTML from "../assets/html.png";
-import CSS from "../assets/css.png";
-import JavaScript from "../assets/javascript.png";
-import ReactImg from "../assets/react.png";
-import Node from "../assets/node.png";
-import FireBase from "../assets/firebase.png";
-import GitHub from "../assets/github.png";
-import Tailwind from "../assets/tailwind.png";
-import Mongo from "../assets/mongo.png";
+import HTML from "../../assets/html.png";
+import CSS from "../../assets/css.png";
+import JavaScript from "../../assets/javascript.png";
+import ReactImg from "../../assets/react.png";
+import ReactNative from "../../assets/react-native.png";
+import TypeScript from "../../assets/typescript.png";
+import Alphine from "../../assets/alphine.png";
+import Filament from "../../assets/filament.png";
+import Livewire from "../../assets/livewire.png";
+import Laravel from "../../assets/laravel.png";
+import Symfony from "../../assets/symfony.png";
+import Node from "../../assets/node.png";
+import FireBase from "../../assets/firebase.png";
+import GitHub from "../../assets/github.png";
+import Tailwind from "../../assets/tailwind.png";
+import Mongo from "../../assets/mongo.png";
 
 export const skillConfig = [
   {
@@ -31,8 +38,43 @@ export const skillConfig = [
   },
   {
     id: 4,
+    value: "REACT Native",
+    image: ReactNative,
+  },
+  {
+    id: 5,
+    value: "TypeScript",
+    image: TypeScript,
+  },
+  {
+    id: 6,
+    value: "Alphine",
+    image: Alphine,
+  },
+  {
+    id: 7,
+    value: "Filament",
+    image: Filament,
+  },
+  {
+    id: 8,
+    value: "Livewire",
+    image: Livewire,
+  },
+  {
+    id: 9,
+    value: "Laravel",
+    image: Laravel,
+  },
+  {
+    id: 10,
+    value: "Symfony",
+    image: Symfony,
+  },
+  {
+    id: 4,
     value: "GitHub",
-    Image: GitHub,
+    image: GitHub,
   },
   {
     id: 5,
