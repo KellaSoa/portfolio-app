e304aa6ba70eb293a0818ac4976daf02305a9bb9
Kella <fkellaie@gmail.com>
2024-12-04 14:59:46 +0100

    🚧  F07 > Fix css

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -2,10 +2,7 @@ import myPhoto from "../../assets/kella-.png";
 
 export default function Contact() {
   return (
-    <div
-      id="contact"
-      className="w-full h-screen flex justify-center items-center p-4"
-    >
+    <div id="contact" className="w-full flex justify-center items-center p-4">
       <div className="pb-8">
         <p className="text-4xl text-yellow font-bold inline border-b-4 border-yellow">
           Contact
@@ -33,23 +30,27 @@ export default function Contact() {
             className="flex flex-col max-w-[600px] w-full"
           >
             <input
-              className="bg-white p-2 text-blue rounded-md border-2 border-blue focus:outline-none focus:ring-2 focus:ring-yellow focus:border-yellow"
+              className="bg-white p-2 text-blue rounded-md border-2 border-blue focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary"
               type="text"
-              placeholder="Name"
+              placeholder="Name*"
               name="name"
+              required
             />
             <input
-              className="my-4 p-2 bg-white text-blue rounded-md border-2 border-blue focus:outline-none focus:ring-2 focus:ring-yellow focus:border-yellow"
+              className="my-4 p-2 bg-white text-blue rounded-md border-2 border-blue focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary"
               type="email"
-              placeholder="Email"
+              placeholder="Email*"
               name="email"
+              required
             />
             <textarea
               className="bg-white p-2 text-blue rounded-md border-2 border-blue focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary"
               name="message"
-              placeholder="Message"
+              placeholder="Message*"
+              required
               rows="10"
             ></textarea>
+            <p className="pt-2 text-blue font-bold "> (*) Required field</p>
             <button className="text-white border-2 hover:bg-pink bg-primary hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
               Let's Collaborate
             </button>
